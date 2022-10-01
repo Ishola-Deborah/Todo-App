18356ed427f1bad6bef5c69640011721f490e89a
DebbDev <82146998+Dhebby-Opeyemi@users.noreply.github.com>
2022-10-01 01:21:35 +0100

    checked button done

--- main.js
+++ main.js
@@ -1,52 +1,64 @@
 //getting all required elements
-const inputField = document.querySelector(".inputField");
-const inputTask = document.querySelector(".input_task");
-const addBtn = document.querySelector(".add_task");
+  const inputField = document.querySelector(".inputField");
+  const inputTask = document.querySelector(".input_task");
+  const addBtn = document.querySelector(".add_task");
+  
+  const addList = document.querySelector(".add_list");
+  const clearAllBtn = document.querySelector(".clear_btn");
 
-const addList = document.querySelector(".add_list");
-const clearAllBtn = document.querySelector(".clear_btn");
+  const checkBox = document.querySelector(".checked");
+  const editBtn = document.querySelector(".edit");
+  console.log(editBtn)
 
-//event listener for addBtn
+ 
 
-addBtn.addEventListener("click", function () {
-  let newItem = document.createElement("div");
+
+  addBtn.addEventListener("click", function () {
+    const newItem = document.createElement("div");
     newItem.classList.add("add_list");
-    newItem.innerHTML = `  <div class="add_list">
+    newItem.innerHTML =  `<div class="add_list">
     <div class="list_item">
       <div class="item_left">
         <input type="checkbox"/>
         <span class="contents">${inputTask.value}</span>
       </div>
       <div class="item_right">
-        <i class="fa-solid fa-pen-to-square"></i>
+        <i class="fa-solid fa-pen-to-square" class="edit" ></i>
         <i class="fa-regular fa-trash-can"></i>
       </div>
     </div>
-  </div>`;
-
+  </div>`
+  
+    // const listItem = document.createElement("div");
+    // listItem.classList.add("list_item");
+   
+  
     // addList.appendChild(newItem);
     // inputTask.value = "";
   
-    if(inputTask.value == ""){
+    if (inputTask.value == "") {
       alert("Please Enter a Task");
-    }else{
+    } else {
       addList.appendChild(newItem);
       inputTask.value = "";
-  
     }
-});
+  
+  });
 
-//delete item
+
+// //delete item
 addList.addEventListener("click", (e) => {
   if(e.target.classList.contains("fa-trash-can")){
     e.target.parentElement.parentElement.remove();
-  } 
-});
-addList.addEventListener("click", (e) => {
-  if(e.target.classList.contains("fa-pen-to-square")){
-    e.target.parentElement.parentElement.classList.toggle("check");
   }
 });
-
+addList.addEventListener("click", (e) => {
+  if(e.target.classList.contains("")){
+    e.target.parentElement.parentElement.classList.toggle("checked");
+  }
+});
+editBtn.addEventListener("click", () => {
+  console.log(editBtn)
+})
 
 
